d69b3e6340fbe52e66ecd8f7fac608c99d7e4471
SmitheScript <36131619+samiti3d@users.noreply.github.com>
2023-04-29 23:35:05 +0700

    Communication with Host

--- WebView2/Form1.Designer.cs
+++ WebView2/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
             this.addressBar = new System.Windows.Forms.TextBox();
             this.goButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,35 +40,46 @@
             this.webView21.AllowExternalDrop = true;
             this.webView21.CreationProperties = null;
             this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView21.Location = new System.Drawing.Point(12, 56);
+            this.webView21.Location = new System.Drawing.Point(12, 102);
             this.webView21.Name = "webView21";
-            this.webView21.Size = new System.Drawing.Size(776, 382);
+            this.webView21.Size = new System.Drawing.Size(776, 336);
             this.webView21.TabIndex = 0;
             this.webView21.ZoomFactor = 1D;
             this.webView21.Click += new System.EventHandler(this.webView21_Click);
             // 
             // addressBar
             // 
-            this.addressBar.Location = new System.Drawing.Point(12, 13);
+            this.addressBar.Location = new System.Drawing.Point(12, 42);
             this.addressBar.Name = "addressBar";
-            this.addressBar.Size = new System.Drawing.Size(541, 26);
+            this.addressBar.Size = new System.Drawing.Size(405, 26);
             this.addressBar.TabIndex = 1;
             // 
             // goButton
             // 
-            this.goButton.Location = new System.Drawing.Point(559, 12);
+            this.goButton.Location = new System.Drawing.Point(443, 35);
             this.goButton.Name = "goButton";
-            this.goButton.Size = new System.Drawing.Size(85, 27);
+            this.goButton.Size = new System.Drawing.Size(85, 40);
             this.goButton.TabIndex = 2;
             this.goButton.Text = "Go!";
             this.goButton.UseVisualStyleBackColor = true;
             this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(544, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.goButton);
             this.Controls.Add(this.addressBar);
             this.Controls.Add(this.webView21);
@@ -85,6 +97,7 @@
         private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
         private System.Windows.Forms.TextBox addressBar;
         private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WebView2/Form1.cs
+++ WebView2/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,15 +21,22 @@ namespace WebView2
             InitializeComponent();
             this.Resize += new System.EventHandler(this.Form_Resize);
             webView21.NavigationStarting += EnsureHttps;
+
             InitializeAsync();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //InitBrowser();
+            Console.WriteLine("Form Load");
 
         }
 
+        private async Task WelcomeWebView(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+   
+           await  webView21.CoreWebView2.ExecuteScriptAsync($"Welcome to web view");
+   
+        }
         void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
         {
             String uri = args.Uri;
@@ -54,15 +62,16 @@ namespace WebView2
         private async void InitializeAsync()
         {
             await webView21.EnsureCoreWebView2Async(null);
+            //Set Default Web Page
+            webView21.CoreWebView2.Navigate("https://www.oremanga.net");
             webView21.CoreWebView2.WebMessageReceived += UpdateAddressBar;
-
             await webView21.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
             await webView21.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => alert(event.data));");
         }
 
-        public async void InitBrowser()
+        public void InitBrowser()
         {
-            //webView21.CoreWebView2.Navigate("https://www.google.com");
+            webView21.CoreWebView2.Navigate("https://www.google.com");
         }
 
         void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
@@ -76,6 +85,7 @@ namespace WebView2
         private void goButton_Click(object sender, EventArgs e)
         {
             Console.WriteLine("GO BUTTON");
+            //webView21.CoreWebView2.ExecuteScriptAsync($"alert('GO!')");
 
             if (webView21 != null && webView21.CoreWebView2 != null)
             {
@@ -84,5 +94,15 @@ namespace WebView2
 
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var script = "alert(document.querySelector(\".header-logo\").innerText);";
+        //await webView.CoreWebView2.ExecuteScriptAsync("(function() { document.getElementById('user').value = 'blahblahusername'; document.getElementById('password').value = 'blahpassword'; })()");
+        //await webView.CoreWebView2.ExecuteScriptAsync("(function() { document.getElementsByName('wp-submit')[0].click(); })()");
+            webView21.CoreWebView2
+                .ExecuteScriptAsync(script);
+        }
     }
 }
+//https://weblog.west-wind.com/posts/2021/Jan/26/Chromium-WebView2-Control-and-NET-to-JavaScript-Interop-Part-2#summary
