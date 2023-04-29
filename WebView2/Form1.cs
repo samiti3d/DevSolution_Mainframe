475ae1102ebe6f2a8f6cb20b1ecdf1339a8c939d
SmitheScript <36131619+samiti3d@users.noreply.github.com>
2023-04-29 17:00:28 +0700

    Add project files.

---
+++ WebView2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+namespace WebView2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async Task initizated()
+        {
+            //checking webview
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+
+        public async void InitBrowser()
+        {
+            await initizated();
+            webView21.CoreWebView2.Navigate("https://www.google.com");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitBrowser();
+        }
+    }
+}
